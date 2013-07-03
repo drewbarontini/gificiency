38bda24eca4270f04c9f089a59e28fed6c02c5d4
Drew Barontini <drewbarontini@gmail.com>
2013-07-03 10:57:13 -0400

    Remove the dot from files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>Gificiency</title>
    <link href="http://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet" type="text/css">
-   <link rel="stylesheet" href=".gificiency/.gificiency.css">
+   <link rel="stylesheet" href="gificiency/gificiency.css">
 </head>
 <body class="cell">
   <form action="">
@@ -13,13 +13,13 @@
   </form>
   <ul>
     <?php
-      require_once '.gificiency/.gificiency.php';
+      require_once 'gificiency/gificiency.php';
       print_files('http://drewb.io/m', '../m');
     ?>
   </ul>
 
   <script src="http://code.jquery.com/jquery-1.10.0.min.js" type="text/javascript"></script>
-  <script src=".gificiency/.gificiency.js" type="text/javascript"></script>
+  <script src="gificiency/gificiency.js" type="text/javascript"></script>
   <script type="text/javascript">
     $(function() {
       Gificiency.init();
