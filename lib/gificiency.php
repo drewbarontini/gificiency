cc23b9fcfd2183f4388fcdc3c730bc4909c3b5c3
Drew Barontini <drewbarontini@gmail.com>
2014-01-16 16:52:16 -0500

    Adjust the error message styling

--- lib/gificiency.php
+++ lib/gificiency.php
@@ -37,7 +37,7 @@ function print_files($url, $dir)
     }
   } else {
     echo("
-      <p class='tac'>Couldn't open the directory specified.</p>
+      <p class='mtm tac'>Couldn't open the directory specified.</p>
     ");
   }
 }
