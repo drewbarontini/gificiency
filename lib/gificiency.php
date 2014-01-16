5c2cde39bb2958e86a66b4815bf6ffcfba87453d
Drew Barontini <drewbarontini@gmail.com>
2014-01-16 16:49:30 -0500

    Add error message if directory can't be read

--- lib/gificiency.php
+++ lib/gificiency.php
@@ -1,33 +1,43 @@
 <?php require_once 'config.php';
 
+if ($env == 'production') {
+  error_reporting(E_ERROR | E_PARSE);
+}
+
 function print_files($url, $dir)
 {
   $hide = ".";
 
   $myDirectory = opendir($dir);
 
-  while ($entryName = readdir($myDirectory))
-  {
-    $dirArray[] = $entryName;
-  }
-
-  closedir($myDirectory);
-
-  $indexCount = count($dirArray);
-
-  sort($dirArray);
-
-  for ($index = 0; $index < $indexCount; $index++)
-  {
-    if (substr("$dirArray[$index]", 0, 1) != $hide)
+  if ($myDirectory) {
+    while ($entryName = readdir($myDirectory))
     {
-      $name = $dirArray[$index];
-      $namehref = $dirArray[$index];
-      echo("
-        <li>
-          <a class='link' href='$url/$namehref'>$name</a>
-        </li>
-      ");
+      $dirArray[] = $entryName;
     }
+
+    closedir($myDirectory);
+
+    $indexCount = count($dirArray);
+
+    sort($dirArray);
+
+    for ($index = 0; $index < $indexCount; $index++)
+    {
+      if (substr("$dirArray[$index]", 0, 1) != $hide)
+      {
+        $name = $dirArray[$index];
+        $namehref = $dirArray[$index];
+        echo("
+          <li>
+            <a class='link' href='$url/$namehref'>$name</a>
+          </li>
+        ");
+      }
+    }
+  } else {
+    echo("
+      <p class='tac'>Couldn't open the directory specified.</p>
+    ");
   }
 }
