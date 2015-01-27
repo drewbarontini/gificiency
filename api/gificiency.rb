de906e111b0fccdb1b9690c097d419453d89d464
Drew Barontini <drewbarontini@gmail.com>
2015-01-27 06:12:50 -0500

    Document and slighlty refactor API class

--- api/gificiency.rb
+++ api/gificiency.rb
@@ -1,39 +1,102 @@
 require 'net/http'
 require 'json'
 
+# ====================================
+#   Gificiency
+# ====================================
+
 class Gificiency
 
+  # Constructor
+  #
+  # == Parameters
+  #
+  # == Usage
+  #
+  #   Gificiency.new
+  #
   def initialize
-    @gifs = {}
-    @categories = []
+    @gifs          = {}
+    @categories    = []
     @category_gifs = []
-
-    @response = get_json
-    @gifs = JSON.parse(@response.body, symbolize_names: true)
+    @response      = get_json
+    @gifs          = JSON.parse(@response.body, symbolize_names: true)
 
     set_categories
   end
 
+  # Get JSON
+  #
+  # == Usage
+  #
+  #   get_json
+  #
+  def get_json
+    Net::HTTP.get_response('gificiency.com', '/gifs.json')
+  end
+
+  # Gets the category from the string
+  #
+  # == Parameters
+  #
+  # @param string [String] the category string
+  #
+  # == Usage
+  #
+  #   get_category('category-name.gif')
+  #
   def get_category(string)
     string.to_s.split('-')[0]
   end
 
+  # Gets the category from an unprefixed string
+  #
+  # == Parameters
+  #
+  # @param string [String] the category string
+  #
+  # == Usage
+  #
+  #   get_category('name.gif')
+  #
   def get_categoryless_gif(string)
     string.to_s.split('.')[0]
   end
 
+  # Get all the unique categories
+  #
+  # == Usage
+  #
+  #   get_categories
+  #
   def get_categories
     @categories.uniq.join(', ')
   end
 
+  # Set the categories
+  #
+  # == Usage
+  #
+  #   set_categories
+  #
   def set_categories
-    @gifs.each do |gif|
+    @categories = @gifs.map do |gif|
       unless gif[:category] == nil
-        @categories << get_category( gif[:category] )
+        get_category( gif[:category] )
       end
     end
   end
 
+  # Get the GIFs based on a category
+  #
+  # == Parameters
+  #
+  # @param category [String] the category
+  #
+  # == Usage
+  #
+  #   get_category_gifs('angry')
+  #
   def get_category_gifs(category)
     category_gifs = []
 
@@ -52,14 +115,26 @@ class Gificiency
     category_gifs
   end
 
-  def get_json
-    Net::HTTP.get_response('gificiency.com', '/gifs.json')
-  end
-
+  # Get a random GIF
+  #
+  # == Usage
+  #
+  #   get_random_gif
+  #
   def get_random_gif
     @gifs.sample[:url]
   end
 
+  # Get a random GIF by category
+  #
+  # == Parameters
+  #
+  # @param category [String] the category
+  #
+  # == Usage
+  #
+  #   get_random_gif_by_category('dealwithit')
+  #
   def get_random_gif_by_category(category)
     gif_set = get_category_gifs(category)
 
